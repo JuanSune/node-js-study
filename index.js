bccd9b23b68af169e115c2a3c5257589c4a8b6a7
Juan Suñe <111535673+JuanSune@users.noreply.github.com>
2022-12-25 21:31:31 -0300

    up

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', function(req, res){
-    res.send('Welcome');
+    res.sendFile(__dirname + '/html/index.html');
 });
+
+
 
 app.listen(8081);
